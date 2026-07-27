0fa2db627e238a81b01d2b67b95407f1afbc23ac
silurven <silurven@gmail.com>
2026-07-27 14:35:42 +0200

    bruh

---
+++ BatLauncher.bat
@@ -0,0 +1,10 @@
+@echo off
+cd ..\..
+IF EXIST kingdoms.exe (start /affinity 1 /high kingdoms.exe @%0\..\Configuration.cfg) ELSE (
+IF EXIST medieval2.exe (start /affinity 1 /high medieval2.exe @%0\..\Configuration.cfg) ELSE (
+    echo.
+    echo ERROR: Cannot find the M2TW or Kingdoms executable.
+    echo.
+    pause
+  )
+)
